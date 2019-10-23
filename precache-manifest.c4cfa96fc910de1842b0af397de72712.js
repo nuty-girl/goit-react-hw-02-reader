7c422a694d7790c04e0d1917798d2924359183b0
unknown <nuty-girl@mail.ru>
2019-10-23 16:29:08 +0300

    Updates

---
+++ precache-manifest.c4cfa96fc910de1842b0af397de72712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544f2e4f713907cc5bc4c13cb6259d06",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "31bf17f4fa06fb93f39c",
+    "url": "/goit-react-hw-02-reader/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "de04470a0b43c8fcd973",
+    "url": "/goit-react-hw-02-reader/static/css/main.6b30f03c.chunk.css"
+  },
+  {
+    "revision": "31bf17f4fa06fb93f39c",
+    "url": "/goit-react-hw-02-reader/static/js/2.1296be1a.chunk.js"
+  },
+  {
+    "revision": "de04470a0b43c8fcd973",
+    "url": "/goit-react-hw-02-reader/static/js/main.8443f1ca.chunk.js"
+  },
+  {
+    "revision": "8cb9d649a7fa951055e2",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.2bac3bd1.js"
+  }
+]);
